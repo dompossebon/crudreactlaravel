d2e6be5055da0210c6c5e584836785fe308adbd8
dompossebon <dompossebon@gmail.com>
2020-01-06 22:26:31 -0300

    ultima revisao

--- resources/assets/js/components/Header.js
+++ resources/assets/js/components/Header.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Header = () => (
     <nav className='navbar navbar-expand-md navbar-light navbar-laravel'>
         <div className='container'>
-                <img src={require("../../../../public/storage/images/logo.gif")} alt="" className="img-responsive"/>
+                <img src={require("../../../../public/images/logo.gif")} alt="" className="img-responsive"/>
             <Link className='navbar-brand' to='/'>CRUD / REACTJS + LARAVEL - Voltar p/ Tela Inicial</Link>
         </div>
     </nav>
